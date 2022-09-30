f4cfa74950dd5ff7a4b193e2196779c576c76d9a
Sam Davis Omekara Jr <osamdavis@gmail.com>
2022-09-29 20:20:30 -0500

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Sam Davis Omekara  on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncImage/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AsyncImage
+//
+//  Created by Sam Davis Omekara  on 9/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
